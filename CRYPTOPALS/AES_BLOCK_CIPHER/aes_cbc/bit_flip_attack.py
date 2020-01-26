c1d1cf93b999cf1bab5dedd4ed9821ff3a25d266
noxious-dervisious <54416977+noxious-dervisious@users.noreply.github.com>
2020-01-26 16:23:22 +0530

    Update bit_flip_attack.py

--- CRYPTOPALS/AES_BLOCK_CIPHER/aes_cbc/bit_flip_attack.py
+++ CRYPTOPALS/AES_BLOCK_CIPHER/aes_cbc/bit_flip_attack.py
@@ -1,19 +1,48 @@
 from Crypto.Cipher import AES
 import os
-from encrypt import encrypt_CBC as enc
-from encrypt import decrypt_CBC as dec
+from pkcs_padding import *
+from hashlib import md5
 
 keylength = 16
 
-key = os.urandom(keylength)
+KEY = os.urandom(keylength)
 IV = os.urandom(keylength)
 
+class AESCipher :
+	def __init__(self,key):
+		self.key = md5(key).digest()
+	def encrypt_CBC(self,payload,IV):
+		lock = AES.new(self.key,AES.MODE_CBC,IV)
+		prepond = "comment1=cooking%20MCs;userdata="
+		postpond = ";comment2=%20like%20a%20pound%20of%20bacon"
+		payload = list(payload)
+		for i in range (len(payload)) :
+			if payload[i] == ';' or payload[i] == '=':
+				payload[i] = "#"
+		payload = ''.join(payload)
+		ciphertext = prepond + payload + postpond
+		ciphertext = padding(ciphertext)
+		ciphertext = lock.encrypt(ciphertext)
+		return ciphertext
+
+	def decrypt_CBC(self,ciphertext,IV):
+		lock = AES.new(self.key,AES.MODE_CBC,IV)
+		plaintext = lock.decrypt(ciphertext)
+		plaintext = unpadding(plaintext)
+		if ";admin=True;" in plaintext:
+			return True
+		else :
+			return False
+
+aescipher = AESCipher(KEY)
 payload = ";admin=True;"
-ciphertext = enc(payload,key,IV)
+ciphertext = aescipher.encrypt_CBC(payload,IV)
 
 def exploit():
+	
 	cipher_block = []
 	global ciphertext
+
 	for i in range (0,len(ciphertext),keylength):
 		cipher_block.append(ciphertext[i:i+keylength])
 	required = list(cipher_block[1])
@@ -24,10 +53,8 @@ def exploit():
 	required = ''.join(required)
 	cipher_block.insert(1,required)
 	ciphertext = ''.join(cipher_block)
-	plaintext = dec(ciphertext,key,IV)
-	if ";admin=True;" in plaintext:
-		print("Logged in as Admin")
+	if aescipher.decrypt_CBC(ciphertext,IV) :
+		print("Welcome Admin")
 	else :
-		print("Sorry try again")
-
+		print("Alert there's an attack")
 exploit()
